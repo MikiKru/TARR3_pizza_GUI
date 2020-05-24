2f3e4a6dbade6d8403152c229fdd1f77d3826752
MikiKru <michal_kruczkowski@o2.pl>
2020-05-24 12:12:26 +0200

    pizza gui v3

--- src/main/java/pizza_gui/Main.java
+++ src/main/java/pizza_gui/Main.java
@@ -10,12 +10,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        // FXMLLoader -> załadowanie pliku .fxml i zwrócenie go jako obiekt Parent
+        Parent root = FXMLLoader.load(getClass().getResource("/view/pizzaView.fxml"));
+        // Utworzenie obiektu Scene wypełnionego wikiem aplikacji
+        Scene scene = new Scene(root);
+        // Ustawienie obiektu Scene w Stage-u (oknie aplikacji)
+        primaryStage.setTitle("Pizza");
+        primaryStage.setScene(scene);
+        // Wyświeltenie i zatrzymanie okna aplikacji
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
